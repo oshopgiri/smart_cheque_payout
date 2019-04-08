3bc05a454bba463c6cd321f8be66df62816efa11
Osho Prakash Giri <oshopgiri@hotmail.com>
2019-04-09 00:40:23 +0530

    user avatar (#1)

--- app/models/document.py
+++ app/models/document.py
@@ -7,18 +7,29 @@ from app.models import User
 
 
 def directory_path(instance, filename):
-    return 'documents/{0}/{1}'.format(instance.user.id, filename)
+    return 'users/{0}/documents/{1}/{2}'.format(instance.user.id, instance.id, filename)
 
 
 class Document(models.Model):
     name = models.CharField(max_length=254)
-    attachment = models.FileField(upload_to=directory_path)
+    attachment = models.FileField(upload_to=directory_path, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            attachment = self.attachment
+            self.attachment = None
+            super(Document, self).save(*args, **kwargs)
+            self.attachment = attachment
+            if 'force_insert' in kwargs:
+                kwargs.pop('force_insert')
+
+        super(Document, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('document_edit', kwargs={'pk': self.pk})
